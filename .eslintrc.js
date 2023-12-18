35a7e899084fd6a3be641a9f66ecfba8f196f5aa
Atte-Oksanen <atteoksanen@outlook.com>
2023-12-18 11:17:10 +0200

    updated lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,8 @@ module.exports = {
     ],
     "linebreak-style":
       ["error",
-        process.env.NODE_ENV === 'prod' ? "unix" : "windows"],
+        "windows"
+      ],
     "quotes": [
       "error",
       "single"
